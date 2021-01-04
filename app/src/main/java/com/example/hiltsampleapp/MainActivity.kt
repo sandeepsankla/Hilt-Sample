c82c7de439856d3bf67bf87a8ecf18e41a07812a
sandeep sankla <sandeep.sankla@myparkplus.com>
2021-01-04 23:18:24 +0530

    added scope

--- app/src/main/java/com/example/hiltsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/hiltsampleapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.hiltsampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+import javax.inject.Singleton
 
-@AndroidEntryPoint // for makking a class to get ready for dependency injected
+@AndroidEntryPoint // for making a class to get ready for dependency injected
 class MainActivity : AppCompatActivity() {
+    val TAG = MainActivity::class.java.simpleName
     //field injection
     @Inject
     lateinit var doSomeThing: DoSomeThing
@@ -14,8 +17,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print(doSomeThing.doSomeThing())
-        print(doSomeThing.doSomeOtherthing())
+        Log.v(TAG, doSomeThing.doSomeThing())
+        Log.v(TAG, doSomeThing.doSomeOtherthing())
     }
 }
 
@@ -35,3 +38,4 @@ class DoSomeOtherThing @Inject constructor(){
         return "Look I did some Other thing"
     }
 }
+
